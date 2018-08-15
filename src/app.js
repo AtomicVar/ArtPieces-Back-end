034c8a1f12b0149800d5c2584d0b1806b655aeac
GuoShuai <msn4399@163.com>
2018-08-15 11:26:20 +0800

    put schema.graphql into separate file

--- src/app.js
+++ src/app.js
@@ -1,89 +1,12 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer } from 'apollo-server';
+import schema from '../data/schema.graphql';
 import * as controller from './controller';
 import { showPrompt } from './misc';
 
 /* Show prompt before starting the server. */
 showPrompt();
 
-const typeDefs = gql`
-  scalar Datetime
-  scalar JSON
-  scalar Image
-  scalar UUID
-  scalar Msg
-  type File {
-    filename: String!
-    mimetype: String!
-    encoding: String!
-  }
-
-  type User {
-    email: String
-    name: String
-    portrait: Image
-    artworks: [Artwork]
-    repos: [Repo]
-  }
-
-  type Artwork {
-    id: UUID
-    keyPhoto: Image
-    title: String
-    description: String
-    creator: String
-    timestamp: Datetime
-    belongingRepo: UUID
-  }
-
-  type Repo {
-    id: UUID
-    keyArtwork: Artwork
-    title: String
-    Starter: User
-    timestamp: Datetime
-    artworks: [Artwork]
-  }
-
-  type Lecture {
-    id: UUID
-    artwork: Artwork
-    title: String
-    description: String
-    timestamp: Datetime
-    creator: User
-    numberOfSteps: Int
-    steps: JSON
-    numberOfStars: Int
-  }
-
-  type Query {
-    getWork(id: UUID!): Artwork
-    getUser(email: String!): User
-    getRepo(id: UUID!): Repo
-    getLecture(id: UUID!): Lecture
-  }
-
-  type Mutation {
-    """
-    Update or insert an user.
-
-    An update will be executed if there is an old user having the same email. Otherwise a new user will be added.
-    
-    Return \`true\` if an insert is executed, \`false\` for an update.
-    """
-    upsertUser(email: String!, name: String!, password: String!, portrait: Image): Boolean
-
-    """
-    Update or insert a work.
-
-    An update will be executed if there is an old work having the same email. Otherwise a new user will be added.
-    
-    Return \`true\` if an insert is executed, \`false\` for an update.
-    """
-    upsertWork(email: Int!, keyPhoto: Image!, title: String!, description: String!, belongingRepo: UUID): Int
-    imgUpload(img: Upload!): File!
-  }
-`;
+const typeDefs = schema;
 
 const resolvers = {
     Query: {
