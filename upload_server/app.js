8c6fef28081ef01130b13ca6bd5aa02ebd15cf3a
GuoShuai <msn4399@163.com>
2018-08-16 21:46:29 +0800

    uploadServer now use formidable

--- upload_server/app.js
+++ upload_server/app.js
@@ -1,32 +1,75 @@
-const Koa = require('koa');
-const logger = require('koa-logger');
-const body = require('koa-better-body');
-const router = require('koa-router')();
-const serve = require('koa-static');
+const formidable = require('formidable');
+const http = require('http');
 const fs = require('fs');
-const path = require('path');
 
-const app = new Koa();
-const APIURL = 'http://127.0.0.1:4001/';
+const uploadServer = http.createServer((req, res) => {
+    // Upload a file
+    if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+        let form = new formidable.IncomingForm();
+        let msg = {};
 
-const upload = async (ctx) => {
-    ctx.response.type = 'application/json';
-    let file = ctx.request.files[0];
-    let newfile = 'files/' + path.basename(file.path) + '.png';
-    fs.copyFile(file.path, newfile, (err) => { if (err) throw err; });
-    ctx.response.body = {
-        msg: 'File saved',
-        url: APIURL + newfile, 
-    };
+        form.on('file', (name, file) => {
+            if (file.type == 'image/png') {
+                msg.msg = `${name} uploaded`;
+                msg.url = file.path;
+                console.log(`File ${file.name} saved to ${file.path}.`);
+            } else {
+                msg.error = 'PNG wanted!';
+                console.log('Bad file type.');
+            }
+        });
 
-};
+        form.on('error', (err) => {
+            msg.error = 'Internal Server Error';
+            throw err;
+        });
 
-router.post('/upload', upload);
+        form.on('end', () => {
+            if (msg.error) {
+                res.statusCode = 500;
+            } else {
+                res.statusCode = 200;
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(msg));
+        });
 
-app.use(logger());
-app.use(body({ formLimit: '10mb' }));
-app.use(router.routes());
-app.use(serve('.'));
+        form.uploadDir = './files';
+        form.keepExtensions = true;
+        form.parse(req);
+    }
+    
+    // Download a file
+    else if (req.url.indexOf('/files') == 0 && req.method.toLowerCase() == 'get') {
+        let filePath = req.url.slice(1);
+        fs.stat(filePath, (err, stat) => {
+            if (err) {
+                if ('ENOENT' == err.code) {
+                    res.statusCode = 404;
+                    res.end('Not Found.');
+                } else {
+                    res.statusCode = 500;
+                    res.end('Internal Server Error.');
+                }
+            } else {
+                res.setHeader('Content-Length', stat.size);
+                let stream = fs.createReadStream(filePath);
+                stream.pipe(res);
+                stream.on('error', () => {
+                    res.statusCode = 500;
+                    res.end('Internal Server Error.');
+                });
+            }
+        });
+    }
+    
+    // Malformed URL
+    else {
+        res.statusCode = 400;
+        res.end('Bad Request.');
+    }
+});
 
-app.listen(4001);
-console.log(`File upload server started at ${APIURL}`);
+uploadServer.listen(4001, 'localhost', () => {
+    console.log('Upload server started...');
+});
